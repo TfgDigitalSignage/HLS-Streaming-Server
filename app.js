dadeecb7eb6374b67363e6dae4838e68c4a43674
Gabriel Gutierrez <gagutier@ucm.es>
2019-03-21 19:45:51 +0100

    Enabled HTTP basic authentication for static resources

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express')
-const HLSServer = require('hls-server')
-const http = require('http')
-
 const app = express()
 const start_server = require('./routes/start-server')
 const stop_server = require('./routes/stop-server')
 
 app.use('/start-server', start_server)
 app.use('/stop-server', stop_server)
+
+app.use('/live', (req, res, next) => {
+
+    // -----------------------------------------------------------------------
+    // authentication middleware
+  
+    const auth = {login: 'user', password: 'password'} // change this
+
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
+  
+    // Verify login and password are set and correct
+    if (!login || !password || login !== auth.login || password !== auth.password) {
+      res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+      res.status(401).send('Authentication required.') // custom message
+      return
+    }
+  
+    // -----------------------------------------------------------------------
+    // Access granted...
+    next()
+})
 
 app.use('/live', express.static('__TEMP/video/', {
     setHeaders: (res,path) =>{
--- routes/start-server.js
+++ routes/start-server.js
@@ -3,10 +3,6 @@ const router = express.Router()
 
 router.get('/', (req,res,next)=>{
     const ffmpeg_command = require('../encoder').command
-    ffmpeg_command.on('error', (err)=>{
-        throw err;
-        ffmpeg_command.kill()
-    })
     ffmpeg_command.output('__TEMP/video/playlist.m3u8').run()
     res.end('<h1>Server started successfully</h1>')
 })
