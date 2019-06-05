a111bbc64604079d4b7a594ed1e27adc93ed0961
Gabriel Gutierrez <gagutier@ucm.es>
2019-06-05 16:24:05 +0200

    Create and merge from master

--- encoder.js
+++ encoder.js
@@ -5,11 +5,12 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 exports.command = ffmpeg().input('/dev/video0').addOptions([
   '-s 640x360',          // 640px width, 360px height output video dimensions
   // '-strict experimental',
-  '-hls_time 4',
-  '-hls_list_size 5',
+  '-framerate 24',
+  '-hls_time 1',
+  '-hls_list_size 4',
   '-hls_flags delete_segments+append_list+split_by_time',
   '-pix_fmt yuv420p',
   '-profile:v high',
   '-level 4.2',
   '-movflags +faststart'
-]).videoBitrate(1024)
+]).videoBitrate(500)
